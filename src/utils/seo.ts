e38b6b129e245f8b6c66885293750f9f142ce964
Luke McDonald <thelukemcdonald@gmail.com>
2022-01-14 11:20:26 -0600

    Remove unused dropdown component.

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -3,7 +3,7 @@ import { initSeo } from 'remix-seo'
 export const { getSeo, getSeoMeta, getSeoLinks } = initSeo({
   // Set defaults that will apply to routes w/o specific SEO tags
   title: 'Luke McDonald',
-  titleTemplate: '%s | Luke McDonald',
+  titleTemplate: '%s — Luke McDonald',
   openGraph: {
     siteName: 'Luke McDonald',
     type: 'website',
