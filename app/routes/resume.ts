eb5c57890163c1199d322eeb0bc8ebc07f8ab382
Luke McDonald <lukemcdonald@users.noreply.github.com>
2024-10-19 16:46:42 -0500

    chore: update link to resume (#12)

--- app/routes/resume.ts
+++ app/routes/resume.ts
@@ -2,6 +2,6 @@ import { redirect } from '@remix-run/node'
 
 export const loader = () => {
   return redirect(
-    'https://docs.google.com/document/d/18Xm2J8RiPVIkU91pYQLI2EtrF-O2DAeXj5LKE3VD4no/edit?usp=sharing'
+    'https://docs.google.com/document/d/1NopjwnwOtwKVqz33w8Glly2SKxfesS3oNKHyU8DO1jM/edit?usp=sharing'
   )
 }
